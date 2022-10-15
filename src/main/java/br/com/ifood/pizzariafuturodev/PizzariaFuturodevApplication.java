3a59c0756599679793e57fae862fe5957808e13c
wagner <wagsantos1985@yahoo.com.br>
2022-10-14 21:09:50 -0300

    initial commit

---
+++ src/main/java/br/com/ifood/pizzariafuturodev/PizzariaFuturodevApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifood.pizzariafuturodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaFuturodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaFuturodevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifood/pizzariafuturodev/PizzariaFuturodevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifood.pizzariafuturodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaFuturodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
